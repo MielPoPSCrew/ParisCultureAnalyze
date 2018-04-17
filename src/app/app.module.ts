3ea0479a4a91c553bcf12f617e7b7e6df1135837
Léo Donny <leo.donny2@gmail.com>
2018-04-17 17:16:35 +0200

    (front) add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+import {
+    MatToolbarModule
+} from '@angular/material';
+
+const MATERIAL_MODULES = [
+    MatToolbarModule
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule,
+        MATERIAL_MODULES
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }

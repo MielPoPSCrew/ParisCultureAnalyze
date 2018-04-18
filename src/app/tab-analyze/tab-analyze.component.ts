87e5a251153336c8257009beb18dda57c13a4bdf
Léo Donny <leo.donny2@gmail.com>
2018-04-18 19:46:26 +0200

    (analyze-tab) try some charts - bar chart doesn't work

--- src/app/tab-analyze/tab-analyze.component.ts
+++ src/app/tab-analyze/tab-analyze.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+import { environment } from '../../environments/environment';
+
+// mock
+import { mockCleanDataAnalyze } from '../../mocks/mockCleanDataAnalyze';
 
 @Component({
     selector: 'app-tab-analyze',
@@ -13,47 +17,69 @@ export class TabAnalyzeComponent implements OnInit {
 
     public chartType = 'pie';
     public baseData = 'events';
+    public env = environment;
+    public chartLabels: string[];
+    public chartData: number[];
+    public chartOptions: any;
+    public mockCleanDataAnalyze = mockCleanDataAnalyze;
+    public formattedData: any;
 
     constructor() { }
 
     ngOnInit() {
-        // originalClick = this.currentChart.chart.legend.onClick;
-        // console.log(this.currentChart);
+        this.chartLabels = this.env.cpList;
+        this.chartOptions = {
+            legend: {
+                position: 'right',
+                labels: {
+                  fontSize: 14
+                }
+            }
+        };
+
+        this.initChartData();
     }
 
-    // tslint:disable-next-line:member-ordering
-    public pieChartLabels: string[] = ['75001', '75002', '75003', '75004', '75005', '75006', '75007', '75008',
-     '75009', '75010', '75011', '75012', '75013', '75014', '75015', '75016', '75017', '75018', '75019', '75020'];
-    // tslint:disable-next-line:member-ordering
-    public pieChartData: number[] = [12, 4, 7, 3, 2, 2, 4, 6, 1, 17, 5, 3, 11, 9, 7, 4, 2, 7, 1, 10];
-
-    // tslint:disable-next-line:member-ordering
-    public chartOptions = {
-        legend: {
-            position: 'right',
-            labels: {
-              fontSize: 14
-            },
-            // onClick: (event, legendItem) => {
-            //    this.filterData(legendItem.text);
-            // }
+    initChartData = () => {
+        if (this.chartType === 'pie' || this.chartType === 'doughnut') {
+            switch (this.baseData) {
+                case 'events': this.chartData = this.mockCleanDataAnalyze.events; break;
+                case 'museums': this.chartData = this.mockCleanDataAnalyze.museums; break;
+                case 'cinemas': this.chartData = this.mockCleanDataAnalyze.cinemas; break;
+            }
         }
-    };
+        if (this.chartType === 'bar') {
+            switch (this.baseData) {
+                case 'events': this.chartData = this.formatDataForBarChart(this.mockCleanDataAnalyze.events); break;
+                case 'museums': this.chartData = this.formatDataForBarChart(this.mockCleanDataAnalyze.museums); break;
+                case 'cinemas': this.chartData = this.formatDataForBarChart(this.mockCleanDataAnalyze.cinemas); break;
+            }
+        }
+    }
 
+    formatDataForBarChart = (data) => {
+        const newFormat = [];
+
+        data.map((d, index) => {
+            const tmpArray = [];
+            data.map((dd, ii) => {
+                if (index === ii) {
+                    tmpArray.push(dd);
+                } else {
+                    tmpArray.push(0);
+                }
+            });
+            newFormat.push(tmpArray);
+        });
+
+        console.log(newFormat);
+        return newFormat;
+    }
     // filterData(toggleFilter) {
     //     console.log(this.currentChart);
     //     this.currentChart.chart.legend.onClick = function(e, legendItem) {
     //         this.original.call(this, e, legendItem);
     //         console.log('HEHEHEHE');
     //     };
-    // }
-
-    // events
-    // public chartClicked(e: any): void {
-    //     console.log(e);
-    // }
-
-    // public chartHovered(e: any): void {
-    //     console.log(e);
     // }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,5 +13,6 @@ export const environment = {
         events: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=evenements-a-paris&rows=10000&facet=tags&facet=placename&facet=department&facet=region&facet=city&facet=date_start&facet=date_end&facet=pricing_info&refine.department=Paris&refine.date_start=2018',
         musea: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-musees-de-france-a-paris&q=cp%3E%3D75001+AND+cp%3C%3D75020&rows=10000&facet=cp',
         cinemas: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=cinemas-a-paris&rows=10000&facet=arrondissement'
-    }
+    },
+    cpList: ['75001', '75002', '75003', '75004', '75005', '75006', '75007', '75008', '75009', '75010', '75011', '75012', '75013', '75014', '75015', '75016', '75017', '75018', '75019', '75020'],
 };

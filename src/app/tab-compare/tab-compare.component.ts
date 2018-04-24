e9f1dde6ddc773a1734bcf65a82e39cc8b8ed242
Léo Donny <leo.donny2@gmail.com>
2018-04-24 10:55:00 +0200

    (front-compare-charts) add compare tab

--- src/app/tab-compare/tab-compare.component.ts
+++ src/app/tab-compare/tab-compare.component.ts
@@ -1,15 +1,110 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+import { environment } from '../../environments/environment';
+
+// mock
+import { mockCleanDataCompare } from '../../mocks/mockCleanDataCompare';
 
 @Component({
-  selector: 'app-tab-compare',
-  templateUrl: './tab-compare.component.html',
-  styleUrls: ['./tab-compare.component.css']
+    selector: 'app-tab-compare',
+    templateUrl: './tab-compare.component.html',
+    styleUrls: ['./tab-compare.component.css']
 })
 export class TabCompareComponent implements OnInit {
 
-  constructor() { }
+    public env = environment;
+    public mockCleanDataCompare = mockCleanDataCompare;
+    public chartsType = 'pie';
+    public dataSelected = ['Events', 'Museums', 'Cinemas'];
+    public quarterSelected_1: string;
+    public quarterSelected_2: string;
+    public dataChart_1: number[];
+    public dataChart_2: number[];
+    public chartsColors: any;
+    public chart1Options: any;
+    public chart2Options: any;
+    public shouldRefresh = true;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.chartsColors = this.env.colorList;
+        this.quarterSelected_1 = this.env.cpList[0];
+        this.quarterSelected_2 = this.env.cpList[1];
+
+        this.initChartsOptions();
+        this.initChartsData();
+    }
+
+    initChartsOptions = () => {
+        this.chart1Options = {
+            legend: {
+                position: 'right',
+                labels: {
+                    fontSize: 14
+                }
+            }
+        };
+        this.chart2Options = {
+            legend: {
+                position: 'left',
+                labels: {
+                    fontSize: 14
+                },
+                display: false
+            }
+        };
+    }
+    
+    initChartsData = () => {
+        // To force refresh...
+        this.shouldRefresh = !this.shouldRefresh;
+
+        this.dataChart_1 = [];
+        this.dataChart_2 = [];
+
+        this.initChartsOptions();
+
+        if (this.chartsType === 'bar') {
+            this.chart1Options.scales = {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            };
+
+            this.chart2Options.scales = {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            };
+
+            this.chart1Options.legend = {
+                display: false
+            };
+        }
+
+        if (this.dataSelected.includes('Events')) {
+            this.dataChart_1.push(this.mockCleanDataCompare[this.quarterSelected_1].events);
+            this.dataChart_2.push(this.mockCleanDataCompare[this.quarterSelected_2].events);
+        }
+
+        if (this.dataSelected.includes('Museums')) {
+            this.dataChart_1.push(this.mockCleanDataCompare[this.quarterSelected_1].museums);
+            this.dataChart_2.push(this.mockCleanDataCompare[this.quarterSelected_2].museums);
+        }
+
+        if (this.dataSelected.includes('Cinemas')) {
+            this.dataChart_1.push(this.mockCleanDataCompare[this.quarterSelected_1].cinemas);
+            this.dataChart_2.push(this.mockCleanDataCompare[this.quarterSelected_2].cinemas);
+        }
+
+        console.log('initChartsData');
+        console.log(this.dataChart_1);
+        console.log(this.dataChart_2);
+    }
 
 }
---
+++ src/mocks/mockCleanDataCompare.ts
@@ -0,0 +1,103 @@
+export const mockCleanDataCompare = {
+    '75001': {
+        events: 4,
+        museums: 5,
+        cinemas: 6
+    },
+    '75002': {
+        events: 6,
+        museums: 7,
+        cinemas: 10
+    },
+    '75003': {
+        events: 2,
+        museums: 7,
+        cinemas: 3
+    },
+    '75004': {
+        events: 8,
+        museums: 5,
+        cinemas: 5
+    },
+    '75005': {
+        events: 4,
+        museums: 2,
+        cinemas: 0
+    },
+    '75006': {
+        events: 3,
+        museums: 0,
+        cinemas: 7
+    },
+    '75007': {
+        events: 6,
+        museums: 4,
+        cinemas: 6
+    },
+    '75008': {
+        events: 4,
+        museums: 4,
+        cinemas: 3
+    },
+    '75009': {
+        events: 10,
+        museums: 3,
+        cinemas: 3
+    },
+    '75010': {
+        events: 12,
+        museums: 3,
+        cinemas: 5
+    },
+    '75011': {
+        events: 15,
+        museums: 2,
+        cinemas: 6
+    },
+    '75012': {
+        events: 7,
+        museums: 18,
+        cinemas: 7
+    },
+    '75013': {
+        events: 7,
+        museums: 3,
+        cinemas: 4
+    },
+    '75014': {
+        events: 8,
+        museums: 3,
+        cinemas: 6
+    },
+    '75015': {
+        events: 4,
+        museums: 4,
+        cinemas: 8
+    },
+    '75016': {
+        events: 5,
+        museums: 6,
+        cinemas: 8
+    },
+    '75017': {
+        events: 3,
+        museums: 1,
+        cinemas: 3
+    },
+    '75018': {
+        events: 7,
+        museums: 9,
+        cinemas: 10
+    },
+    '75019': {
+        events: 13,
+        museums: 6,
+        cinemas: 8
+    },
+    '75020': {
+        events: 8,
+        museums: 2,
+        cinemas: 12
+    }
+};
+

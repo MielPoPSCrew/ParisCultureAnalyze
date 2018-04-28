702a2b9912007e87357359afe72de42b7184d4ee
Romain Laneuville <romain.laneuville@hotmail.fr>
2018-04-28 14:12:59 +0200

    [map] + regroups events with MarkerClusterer + icons

--- src/app/tab-map/tab-map.component.ts
+++ src/app/tab-map/tab-map.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ViewChild } from '@angular/core';
-import { } from '@types/googlemaps';
+import {Component, OnInit, Input} from '@angular/core';
+import {ViewChild} from '@angular/core';
+import {} from '@types/googlemaps';
+import _forEach from 'lodash-es/forEach';
+
+// mock
+import {mockCleanData} from '../../mocks/mockCleanData';
 
 // Services
-import { ParisCultureAnalyse } from '../services/paris-culture.service';
+import {ParisCultureAnalyse} from '../services/paris-culture.service';
 
 @Component({
     selector: 'app-tab-map',
     templateUrl: './tab-map.component.html',
     styleUrls: ['./tab-map.component.scss']
 })
+
 export class TabMapComponent implements OnInit {
 
     @Input('initialData') initialData: ParisCultureAnalyse;
@@ -21,13 +26,50 @@ export class TabMapComponent implements OnInit {
 
     ngOnInit() {
         // Show  the google map on the center of Paris
-        const mapProp = {
-            center: new google.maps.LatLng(48.866667, 2.333333),
-            zoom: 12,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+        const markers = [],
+            mapProp = {
+                center: new google.maps.LatLng(48.866667, 2.333333),
+                zoom: 12,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            images = {
+                museum: {
+                    url: 'assets/images/museum.svg',
+                    size: new google.maps.Size(50, 50),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(25, 50)
+                },
+                cinema: {
+                    url: 'assets/images/movie-theater.svg',
+                    size: new google.maps.Size(50, 50),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(25, 50)
+                },
+                event: {
+                    url: 'assets/images/embassy.svg',
+                    size: new google.maps.Size(50, 50),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(0, 50)
+                }
+            };
 
         this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-    }
 
+        _forEach(mockCleanData.data, (event) => {
+            const marker = new google.maps.Marker({
+                position: {lat: event.coords.y, lng: event.coords.x},
+                title: event.title,
+                icon: images[event.type]
+            });
+
+            marker.setMap(this.map);
+            markers.push(marker);
+        });
+
+        const markerCluster = new MarkerClusterer(
+            this.map,
+            markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'}
+        );
+    }
 }
---
+++ src/mocks/mockCleanData.ts
@@ -0,0 +1,33 @@
+export const mockCleanData = {
+    data: [{
+        title: 'Réunion des familles Notre-Dame',
+        type: 'event',
+        dp: 75001,
+        coords: {
+            x: 2.403524,
+            y: 48.845708
+        },
+        link: 'https://openagenda.com/notredamedubonconseil/events/reunion-des-familles-notre-dame_723',
+        info: '2 jours de rencontres et de partages, 2 jours de rencontres et de partages'
+    }, {
+        title: 'test musuem',
+        type: 'museum',
+        dp: 75003,
+        coords: {
+            x: 2.403524,
+            y: 48.86
+        },
+        link: 'https://openagenda.com/notredamedubonconseil/events/reunion-des-familles-notre-dame_723',
+        info: '2 jours de rencontres et de partages, 2 jours de rencontres et de partages'
+    }, {
+        title: 'test cinema',
+        type: 'cinema',
+        dp: 75003,
+        coords: {
+            x: 2.40,
+            y: 48.847
+        },
+        link: 'https://openagenda.com/notredamedubonconseil/events/reunion-des-familles-notre-dame_723',
+        info: '2 jours de rencontres et de partages, 2 jours de rencontres et de partages'
+    }]
+};
